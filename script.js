87e9591f8df359eaeb682e59b2b59a839fd56585
Savanna <149965311+vannatatro@users.noreply.github.com>
2024-03-03 14:20:56 -0500

    Tweak CSS and condensed player name JS

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('working')
+// Game
+
+
+
+
+
+
+
+// Status
+
+
+
+
+// Player Names
+let playerNames = document.getElementById('playerNames');
+playerNames.addEventListener('click', () => {
+    let dialog = document.querySelector('dialog');
+    dialog.showModal()
+    let submitBtn = document.getElementById("submitBtn");
+    submitBtn.addEventListener('click', (event) => {
+        event.preventDefault()
+        let form = document.getElementById('form')
+        const inputp1 = document.querySelector('#p1').value;
+        const inputp2 = document.querySelector('#p2').value;
+        let textp1 = document.querySelector('.play1name');
+        textp1.textContent = inputp1;
+        let textp2 = document.getElementById('play2name');
+        textp2.textContent = inputp2;
+        dialog.close()
+        form.reset()
+    })
+})
+
+// New Game
